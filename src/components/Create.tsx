73a53a59ba04e244a8c30b8027b8c68a841edc97
Thiago LS <61521260+thiago-2005@users.noreply.github.com>
2023-05-25 19:54:14 +0000

    asdadasd

--- src/components/Create.tsx
+++ src/components/Create.tsx
@@ -1,99 +1,99 @@
-"use client"
+// "use client"
 
-import React, { useState, FormEvent, ChangeEvent } from "react";
-import { MapContainer as Map, Marker, TileLayer, AttributionControl, useMapEvents } from 'react-leaflet';
-import { api } from "@/services/api";
-import Leaflet, { LeafletMouseEvent } from "leaflet";
+// import React, { useState, FormEvent, ChangeEvent } from "react";
+// import { MapContainer as Map, Marker, TileLayer, AttributionControl, useMapEvents } from 'react-leaflet';
+// import { api } from "@/services/api";
+// import Leaflet, { LeafletMouseEvent } from "leaflet";
 import Link from "next/link";
-import { useRouter } from "next/navigation";
+// import { useRouter } from "next/navigation";
 
-const mapIcon = Leaflet.icon({
-  iconUrl: 'mark.svg',
-  iconSize: [58, 68],
-  iconAnchor: [29, 68],
-  popupAnchor: [0, -60],
-})
+// const mapIcon = Leaflet.icon({
+//   iconUrl: 'mark.svg',
+//   iconSize: [58, 68],
+//   iconAnchor: [29, 68],
+//   popupAnchor: [0, -60],
+// })
 
 function Create() {
-  const [position, setPosition] = useState({ latitude: 0, longitude: 0 })
-  const [name, setName] = useState('');
-  const [about, setAbout] = useState('');
-  const [images, setImages] = useState<File[]>([]);
-  const [previewImages, setPreviewImages] = useState<string[]>([]);
+  // const [position, setPosition] = useState({ latitude: 0, longitude: 0 })
+  // const [name, setName] = useState('');
+  // const [about, setAbout] = useState('');
+  // const [images, setImages] = useState<File[]>([]);
+  // const [previewImages, setPreviewImages] = useState<string[]>([]);
 
-  const router = useRouter()
+  // const router = useRouter()
 
-  function handleSelectImages(event: ChangeEvent<HTMLInputElement>) {
-    if (!event.target.files) {
-      return;
-    }
+  // function handleSelectImages(event: ChangeEvent<HTMLInputElement>) {
+  //   if (!event.target.files) {
+  //     return;
+  //   }
 
-    console.log(event.target.files)
+  //   console.log(event.target.files)
 
-    const selectedImages = Array.from(event.target.files);
+  //   const selectedImages = Array.from(event.target.files);
 
-    setImages((image) => ([...image, selectedImages[0]]));
+  //   setImages((image) => ([...image, selectedImages[0]]));
 
-    const selectedImagesPreview = selectedImages.map(image => {
-      return URL.createObjectURL(image)
-    });
+  //   const selectedImagesPreview = selectedImages.map(image => {
+  //     return URL.createObjectURL(image)
+  //   });
 
-    setPreviewImages((image) => ([...image, selectedImagesPreview[0]]));
-  }
+  //   setPreviewImages((image) => ([...image, selectedImagesPreview[0]]));
+  // }
 
-  function handleRemoveImage(index: number) {
-    const newImages = [...images];
-    newImages.splice(index, 1);
-    setImages(newImages);
+  // function handleRemoveImage(index: number) {
+  //   const newImages = [...images];
+  //   newImages.splice(index, 1);
+  //   setImages(newImages);
 
-    const selectedImagesPreview = newImages.map(image => {
-      return URL.createObjectURL(image)
-    });
+  //   const selectedImagesPreview = newImages.map(image => {
+  //     return URL.createObjectURL(image)
+  //   });
 
-    setPreviewImages(selectedImagesPreview);
-  }
+  //   setPreviewImages(selectedImagesPreview);
+  // }
 
-  async function handleSubmit(e: FormEvent) {
-    e.preventDefault()
-    const {latitude, longitude} = position;
+  // async function handleSubmit(e: FormEvent) {
+  //   e.preventDefault()
+  //   const {latitude, longitude} = position;
 
-    const data = new FormData();
+  //   const data = new FormData();
 
-    data.append('name', name)
-    data.append('history', about)
-    data.append('longitude', String(longitude))
-    data.append('latitude', String(latitude))
+  //   data.append('name', name)
+  //   data.append('history', about)
+  //   data.append('longitude', String(longitude))
+  //   data.append('latitude', String(latitude))
   
-    images.forEach(image => {
-      data.append('file', image)
-    });
+  //   images.forEach(image => {
+  //     data.append('file', image)
+  //   });
 
-    console.log(data.getAll('longitude'))
-    console.log(data.getAll('latitude'))
+  //   console.log(data.getAll('longitude'))
+  //   console.log(data.getAll('latitude'))
 
-    await api.post('/api/landmark', data);
+  //   await api.post('/api/landmark', data);
 
-    router.push('/locais')
-  }
+  //   router.push('/locais')
+  // }
 
-  const handle: any = (map: any) => {
-    console.log(map);
-    map.target.on("click", function (e: any) {
-      // const { lat, lng } = e.latlng;
-      // L.marker([lat, lng], { icon }).addTo(map.target);
-      const {latlng: {lat: latitude}, latlng: {lng: longitude}} = e;
+  // const handle: any = (map: any) => {
+  //   console.log(map);
+  //   map.target.on("click", function (e: any) {
+  //     // const { lat, lng } = e.latlng;
+  //     // L.marker([lat, lng], { icon }).addTo(map.target);
+  //     const {latlng: {lat: latitude}, latlng: {lng: longitude}} = e;
 
-      setPosition({
-        latitude: latitude,
-        longitude: longitude,
-      })
-    });
-  }
+  //     setPosition({
+  //       latitude: latitude,
+  //       longitude: longitude,
+  //     })
+  //   });
+  // }
 
   return (
-    <div id="page-create-orphanage">
+    <div id="page-create-landmark">
       <main>
-        <form onSubmit={handleSubmit} className="create-orphanage-form">
+        <form className="create-landmark-form">
           <Link href="/locais" className=" hover:bg-orange-300 duration-200 group flex gap-2 mb-3 w-40 items-center rounded-md bg-orange-400 text-white text-sm font-medium pl-2 pr-3 py-2 shadow-sm">
             <svg width="20" height="20" fill="currentColor" className="rotate-180" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
               <path d="M438.6 278.6c12.5-12.5 12.5-32.8 0-45.3l-160-160c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L338.8 224 32 224c-17.7 0-32 14.3-32 32s14.3 32 32 32l306.7 0L233.4 393.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0l160-160z"/>
@@ -102,9 +102,9 @@ function Create() {
             Voltar
           </Link>
           <fieldset>
-            <legend>Adicionar ponto</legend>
+            <legend>Pàgina em mantenção</legend>
 
-            <Map 
+            {/* <Map 
               center={[-26.3055339,-48.850644]} 
               style={{ width: '100%', height: 280 }}
               zoom={15}
@@ -164,11 +164,11 @@ function Create() {
 
               <input multiple onChange={handleSelectImages} type="file" id="image[]" />
             </div>
+          */}
           </fieldset>
-
-          <button className="confirm-button" type="submit">
+          {/*<button className="confirm-button" type="submit">
             Confirmar
-          </button>
+          </button> */}
         </form>
       </main>
     </div>
